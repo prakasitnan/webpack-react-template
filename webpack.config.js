06cf329706afb1fca80ee986f41e9b6f5ba268ab
B19r3d4456 <prakasit.nan@gmail.com>
2020-02-19 11:27:36 +0700

    add react project

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const { resolve } = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test:/\.(js|jsx)$/,
+                exclude: /(node_modules)/,
+                use:{
+                    loader:"babel-loader"
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.(css|scss)$/,
+                exclude: /(node_modules)/,
+                use:[
+                    {loader: 'style-loader'},
+                    {loader: 'css-loader'},
+                    {loader: 'sass-loader'}
+                ]
+            },
+            {
+                test:/\.(png|jpg)$/,
+                exclude: /(node_modules)/,
+                use:[
+                    { loader: 'file-loader'}
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        })
+    ]
+};
